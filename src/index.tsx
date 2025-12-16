c155a2bd220c293a085bcac7508eeb5bf88c4e38
gangsuckmin <kksm021@gmail.com>
2025-12-16 13:37:16 +0900

    modify

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./router/AppRouter";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
     </React.StrictMode>
 );
-
-reportWebVitals();
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import RequireAuth from "./RequireAuth";
 import Layout from "../layout/Layout";
 
@@ -8,26 +8,23 @@ import Popular from "../pages/Popular/Popular";
 import Search from "../pages/Search/Search";
 import Wishlist from "../pages/Wishlist/Wishlist";
 
-export default function AppRouter()
-{
+export default function AppRouter() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/signin" element={<SignIn />} />
+        <Routes>
+            <Route path="/signin" element={<SignIn />} />
 
-                <Route
-                    element={
-                        <RequireAuth>
-                            <Layout />
-                        </RequireAuth>
-                    }
-                >
-                    <Route path="/" element={<Home />} />
-                    <Route path="/popular" element={<Popular />} />
-                    <Route path="/search" element={<Search />} />
-                    <Route path="/wishlist" element={<Wishlist />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+            <Route
+                element={
+                    <RequireAuth>
+                        <Layout />
+                    </RequireAuth>
+                }
+            >
+                <Route path="/" element={<Home />} />
+                <Route path="/popular" element={<Popular />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/wishlist" element={<Wishlist />} />
+            </Route>
+        </Routes>
     );
 }
